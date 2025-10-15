bfc4ef14428746104f90f81b3ae520a49ca25674
rizkidwimartanto <rizkidwimartanto@gmail.com>
2025-10-15 12:49:58 +0700

    commit iki rak sido

--- app/Http/Controllers/LaporanHarianController.php
+++ app/Http/Controllers/LaporanHarianController.php
@@ -8,49 +8,55 @@ use Illuminate\Http\Request;
 
 class LaporanHarianController extends Controller
 {
-    public function index()
-    {
-        $laporanHariIni = LaporanHarianModel::whereDate('created_at', Carbon::today())->get();
+  public function index()
+  {
+    Carbon::setLocale('id');
+    $data = [
+      'laporanHariIni' => LaporanHarianModel::whereDate('created_at', Carbon::today())->get(),
+      'tanggal_sekarang' => Carbon::today()->translatedFormat('l, d F Y')
+    ];
+    return view('index', $data);
+  }
+  public function admin()
+  {
+    $laporanharian = LaporanHarianModel::all();
 
-        return view('index', compact('laporanHariIni'));
-    }
-    public function admin(){
-      $laporanharian = LaporanHarianModel::all();
+    return view('admin', compact('laporanharian'));
+  }
+  public function create()
+  {
+    return view('tambah_laporan');
+  }
+  public function store(Request $request)
+  {
+    // Validasi input
+    $request->validate([
+      'cuaca' => 'required|string',
+      'total_wo' => 'required|string',
+      'total_vcc' => 'required|string',
+      'yantek_performance' => 'required|string',
+      'rating_plnmobile' => 'required|string',
+      'rpt_rct' => 'required|string',
+      'kinerja_pmt' => 'required|string',
+      'anomali_wo' => 'required|string',
+      'safety_performance' => 'required|string',
+      'tidak_padam' => 'required|string',
+      'padam' => 'required|string',
+      'beban_sistem' => 'required|string',
+      'target_p2tl' => 'required|string',
+      'realisasi_p2tl' => 'required|string',
+      'top_performance' => 'required|string',
+      'bottom_performance' => 'required|string',
+      'ganti_meter_tua_prabayar' => 'required|string',
+      'ganti_meter_tua_pascabayar' => 'required|string',
+      'saldo_meter_tua' => 'required|string',
+      'pelanggan_vvip' => 'required|string',
+      'pelanggan_vip' => 'required|string',
+      'pelanggan_prioritas' => 'required|string',
+    ]);
 
-      return view('admin', compact('laporanharian'));
-    }
-    public function create(){
-      return view('tambah_laporan');
-    }
-    public function store(Request $request){
-      // Validasi input
-      $request->validate([
-          'cuaca' => 'required|string',
-          'total_wo' => 'required|string',
-          'total_vcc' => 'required|string',
-          'yantek_performance' => 'required|string',
-          'rating_plnmobile' => 'required|string',
-          'rpt_rct' => 'required|string',
-          'kinerja_pmt' => 'required|string',
-          'anomali_wo' => 'required|string',
-          'safety_performance' => 'required|string',
-          'tidak_padam' => 'required|string',
-          'padam' => 'required|string',
-          'beban_sistem' => 'required|string',
-          'target_p2tl' => 'required|string',
-          'realisasi_p2tl' => 'required|string',
-          'top_performance' => 'required|string',
-          'bottom_performance' => 'required|string',
-          'ganti_meter_tua_prabayar' => 'required|string',
-          'ganti_meter_tua_pascabayar' => 'required|string',
-          'saldo_meter_tua' => 'required|string',
-          'pelanggan_vvip' => 'required|string',
-          'pelanggan_vip' => 'required|string',
-          'pelanggan_prioritas' => 'required|string',
-      ]);
+    LaporanHarianModel::create($request->all());
 
-      LaporanHarianModel::create($request->all());
-
-      return redirect()->route('laporan-harian.index')->with('success', 'Laporan harian berhasil ditambahkan.');
-    }
+    return redirect()->route('laporan-harian.index')->with('success', 'Laporan harian berhasil ditambahkan.');
+  }
 }
